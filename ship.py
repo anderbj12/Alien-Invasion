b5f4638c6abe8e603d429314ac078505434f8b06
Skatuz <anderbj12@gmail.com>
2024-11-25 13:07:18 -0600

    Ahora también se puede mover hacia la izquierda

--- ship.py
+++ ship.py
@@ -15,13 +15,16 @@ class Ship:
         # Start each ship at its current location.
         self.rect.midbottom = self.screen_rect.midbottom
 
-        # Movement flag.
+        # Movement flags.
         self.moving_right = False
+        self.moving_left = False
 
     def update(self):
         """Update the ship´s position based on the movement flag."""
         if self.moving_right:
             self.rect.x += 1
+        if self.moving_left:
+            self.rect.x -= 1
     
     def blitme(self):
         """Draw the ship at its current location."""
