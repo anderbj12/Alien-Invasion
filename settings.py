20a50342c97e40ab9d2ec67b9bc4a9e7476626f7
Skatuz <anderbj12@gmail.com>
2024-11-25 14:54:05 -0600

    Ahora la nave no puede atravesar los bordes de la pantalla

--- settings.py
+++ settings.py
@@ -3,7 +3,10 @@ class Settings:
 
     def __init__(self):
         """Initialize the game´s settings."""
-        #Screen settings
+        # Screen settings
         self.screen_width = 1200
         self.screen_height = 700
         self.bg_color = (230, 230, 230)
+
+        # Ship settings
+        self.ship_speed = 1.5
